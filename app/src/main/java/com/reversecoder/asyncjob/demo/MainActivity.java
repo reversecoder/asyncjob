e2e1091cf6c09f3825ccc17af901bb2d636f7291
Md. Rashadul Alam <rashedul.alam@bjitgroup.com>
2021-08-18 17:12:48 +0600

    Initialized project

---
+++ app/src/main/java/com/reversecoder/asyncjob/demo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.reversecoder.asyncjob.demo;
+
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("CheckThread", "CheckThread>>onCreate>>UI thread: " + Looper.getMainLooper().getThread().getId() + "");
+        boolean isOnUiThread = Thread.currentThread() == Looper.getMainLooper().getThread();
+        Log.d("CheckThread", "CheckThread>>onCreate>>isOnUiThread: " + isOnUiThread);
+
+//        Crouton crouton = new Crouton();
+//        JobWrapper jobWrapper = crouton.async(new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.d("CheckThread", "CheckThread>>onCreate>>jobWrapper>>BG thread: " + Thread.currentThread().getId() + "");
+//                boolean isOnUiThread = Thread.currentThread() == Looper.getMainLooper().getThread();
+//                Log.d("CheckThread", "CheckThread>>onCreate>>jobWrapper>>isOnUiThread: " + isOnUiThread);
+//
+//                ((TextView) findViewById(R.id.tv_text)).setText("Rashed");
+//            }
+//        });
+//        jobWrapper.stop();
+    }
+}
